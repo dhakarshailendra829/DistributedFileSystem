114fd38fe8c3798a560ae6b4e1bb981093d79aca
Shailendra <dhakarshailendra829@gmail.com>
2025-03-28 12:04:11 +0530

    Updated port values

--- project.py
+++ project.py
@@ -123,4 +123,4 @@ def balance_load():
     return jsonify({"message": "Load is balanced!"})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
